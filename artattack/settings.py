c570fe9c3fbb13747f7c1b8327dd7f0a4f8b1d57
zarin313 <zbhuiyan171120@bscse.uiu.ac.bd>
2021-08-06 17:22:36 +0600

    updated settings.py

--- artattack/settings.py
+++ artattack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'znlfn!o&aqozyqdp$w-a4(1fwt)9nb8sibl3dt3a6&-cz3$2_w'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
